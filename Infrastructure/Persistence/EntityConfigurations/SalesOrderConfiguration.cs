a1763789b4fba86f17ed2d095a6a99fbef5c9ce9
Hans Adrianus <hans.adrianus@outlook.com>
2023-01-23 10:01:14 +0700

    Added entity configuration for SalesOrder header and detail

---
+++ Infrastructure/Persistence/EntityConfigurations/SalesOrderConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Persistence.EntityConfigurations
+{
+    public class SalesOrderConfiguration : IEntityTypeConfiguration<SalesOrderHeader>, IEntityTypeConfiguration<SalesOrderDetail>
+    {
+        public void Configure(EntityTypeBuilder<SalesOrderHeader> builder)
+        {
+            builder.ToTable("SalesOrderHeader");
+        }
+
+        public void Configure(EntityTypeBuilder<SalesOrderDetail> builder)
+        {
+            builder.ToTable("SalesOrderDetail");
+        }
+    }
+}
